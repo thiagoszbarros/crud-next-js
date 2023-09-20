5da4a3e1eca73527a106c079431c117a6f411da7
Thiago Barros <thiagobarros95@gmail.com>
2023-09-19 23:31:00 -0300

    chore: conclui dockerização

--- config.tsx
+++ config.tsx
@@ -1,3 +1,3 @@
-const apiUrl = process.env.API_URL ?? 'http://localhost:8001'
+export const API_URL = process.env.API_URL ?? 'http://nginx';
+export const CLIENT_API_URL = process.env.CLIENT_API_URL ?? 'http://127.0.0.1:8001';
 
-export const API_URL = apiUrl;
